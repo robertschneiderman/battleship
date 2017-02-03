c31cfde58f46656d5385ba8be3e2502555b2cf23
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-02-03 11:50:30 -0800

    fixed mongoose connection bug

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 // DB Setup
 mongoose.Promise = global.Promise;
 var env = process.env.NODE_ENV || 'development';
-    mongoose.connect('mongodb://localhost/battleship');
 if (env === 'development') {
+    mongoose.connect('mongodb://localhost/battleship');
 } else {
     mongoose.connect('mongodb://heroku_z0p79sc8:u408971i0bgdka4ban6efmrifo@ds139969.mlab.com:39969/heroku_z0p79sc8');
 }
