f7321c532de48808f02927c65ffc93c014fc4b53
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-02-03 11:47:34 -0800

    changed mongo production db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 // DB Setup
 mongoose.Promise = global.Promise;
 var env = process.env.NODE_ENV || 'development';
-
     mongoose.connect('mongodb://localhost/battleship');
 if (env === 'development') {
 } else {
-    // mongoose.connect('mongodb://heroku_59ldxkrj:15snblhm54fe90hnog8k3dql77@ds113668.mlab.com:13668/heroku_59ldxkrj');
+    mongoose.connect('mongodb://heroku_z0p79sc8:u408971i0bgdka4ban6efmrifo@ds139969.mlab.com:39969/heroku_z0p79sc8');
 }
+
 
 module.exports = { mongoose };
